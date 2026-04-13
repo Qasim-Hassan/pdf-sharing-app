ee88405aee7015c54858f5341a514691c3d3ffde
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-04-13 18:20:16 +0500

    styled-create-post-page

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import {API} from "../api/api"
 import { useNavigate } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { Container, Box, TextField, Button, Typography, Paper } from "@mui/material";
 
 
 const CreatePost = () => {
@@ -30,36 +31,57 @@ const CreatePost = () => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
-      <h1>Create Post</h1>
+      <Container maxWidth="sm" sx={{ mt: 4 }}>
+        <Paper sx={{ p: 4, borderRadius: 3 }}>
+          <Typography variant="h5" sx={{ mb: 3 }}>
+            Create Post
+          </Typography>
 
-      <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <TextField
+                label="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                fullWidth
+              />
 
-        <input
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+              <Button
+                variant="outlined"
+                component="label"
+              >
+                Upload PDF
+                <input
+                  type="file"
+                  accept="application/pdf"
+                  hidden
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFile(e.target.files[0]);
+                    }
+                  }}
+                />
+              </Button>
 
-        <br />
-
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={(e) => {
-            if (e.target.files) {
-              setFile(e.target.files[0])
-            }
-          }}
-        />
-
-        <br />
-
-        <button>Create</button>
-
-      </form>
-
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+              >
+                Create
+              </Button>
+            </Box>
+          </form>
+        </Paper>
+      </Container>
     </div>
   )
 }
