ed84a07b899b3ec92b3c485a31ca2b95124057b2
Qasim Hassan <qasimhassan1947@gmail.com>
2026-04-20 18:24:00 +0000

    added direct db_url link for better testing

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,7 +4,7 @@ from .config import settings
 
 #we have to pass → 'postgresql://<username>:<password>@<ip-address/hostname>/<database-name>'
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
-
+#SQLALCHEMY_DATABASE_URL = f'{settings.db_url}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
